c9e6b3dd6adef767a09fa44f863d8717d8260bad
yurii9132 <author8132@gmail.com>
2024-10-22 17:05:19 +0100

    add reddit-lence app

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-const projectTea = document.getElementById('project-tea');
-const projectSystem = document.getElementById('project-system');
-const projectOil = document.getElementById('project-oil');
-const infoTea = document.getElementById('info-tea');
-const infoSystem = document.getElementById('info-system');
-const infoOil = document.getElementById('info-oil');
-infoTea.style.display = 'none';
-infoSystem.style.display = 'none';
-infoOil.style.display = 'none';
+const projectTea = document.getElementById("project-tea");
+const projectSystem = document.getElementById("reddit-lence");
+const projectOil = document.getElementById("project-oil");
+const infoTea = document.getElementById("info-tea");
+const infoSystem = document.getElementById("reddit-lence-info");
+const infoOil = document.getElementById("info-oil");
+infoTea.style.display = "none";
+infoSystem.style.display = "none";
+infoOil.style.display = "none";
 
-
-
-projectTea.addEventListener('mouseover', () => {
-    infoTea.style.display = 'block';
+projectTea.addEventListener("mouseover", () => {
+  infoTea.style.display = "block";
 });
-projectTea.addEventListener('mouseout', () => {
-    infoTea.style.display = 'none';
+projectTea.addEventListener("mouseout", () => {
+  infoTea.style.display = "none";
 });
 
-projectSystem.addEventListener('mouseover', () => {
-    infoSystem.style.display = 'block';
+projectSystem.addEventListener("mouseover", () => {
+  infoSystem.style.display = "block";
 });
-projectSystem.addEventListener('mouseout', () => {
-    infoSystem.style.display = 'none';
+projectSystem.addEventListener("mouseout", () => {
+  infoSystem.style.display = "none";
 });
 
-projectOil.addEventListener('mouseover', () => {
-    infoOil.style.display = 'block';
-}); 
-projectOil.addEventListener('mouseout', () => {
-    infoOil.style.display = 'none';
+projectOil.addEventListener("mouseover", () => {
+  infoOil.style.display = "block";
+});
+projectOil.addEventListener("mouseout", () => {
+  infoOil.style.display = "none";
 });
 
 // infoTea.addEventListener('mouseover', () => {
@@ -48,6 +46,6 @@ projectOil.addEventListener('mouseout', () => {
 // infoOil.addEventListener('mouseover', () => {
 //     infoOil.style.display = 'block';
 // });
-// infoOil.addEventListener('mouseout', () => {    
+// infoOil.addEventListener('mouseout', () => {
 //     infoOil.style.display = 'none';
 // });
